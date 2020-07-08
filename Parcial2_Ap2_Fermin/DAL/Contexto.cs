64fce8b37c07c84067addbf81cd2e0198442c966
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-07-08 09:35:58 -0400

    Creando contexto

---
+++ Parcial2_Ap2_Fermin/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_Ap2_Fermin.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
+        {
+            dbContext.UseSqlite(@"Data Source = DATA\SegundoParcial.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            
+        }
+    }
+}
